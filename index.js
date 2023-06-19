3ff92906148429a086f7254f46fc296743bd9921
Victor <victornjoroge2020@gmail.com>
2023-06-19 19:29:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+const superbowlWin= (games) =>{
+    let data=games.find((matches) =>{
+        return matches.result === `W`;
+    })
+    if(data){
+        return data.year;
+    }else{
+        return undefined;
+    }
+}
